23aa104853a1b325bf9aea44a583f50de1d6e2f0
Govind Rajput <govindrajput9819@gmail.com>
2024-05-14 18:48:58 +0530

    Copy to clipBoard Added

--- 2.random-color-generator/main.js
+++ 2.random-color-generator/main.js
@@ -1,6 +1,7 @@
 const HexBtn = document.querySelector(".hex-btn");
 const hexColorValue = document.querySelector(".hex-color-value");
 const hexColorContainer = document.querySelector(".hex-color-container");
+const hexCopyBtn = document.querySelector(".hex-copy-color");
 
 //create hex color
 HexBtn.addEventListener("click", () => {
@@ -12,10 +13,11 @@ HexBtn.addEventListener("click", () => {
       Math.floor(Math.random() * charSetLength)
     );
   }
-  hexColorValue.textContent = `#${hexColorOutput}`;
-  hexColorContainer.style.backgroundColor = `#${hexColorOutput}`;
-  HexBtn.style.color = `#${hexColorOutput}`;
-  console.log(hexColorOutput);
+
+  const hexColor = `#${hexColorOutput}`;
+  hexColorValue.textContent = hexColor;
+  hexColorContainer.style.backgroundColor = hexColor;
+  HexBtn.style.color = hexColor;
 });
 
 //RGB Color Generator
@@ -26,6 +28,7 @@ const getGreenInputRange = document.querySelector("#green");
 const getBlueInputRange = document.querySelector("#blue");
 const rgbColorContainer = document.querySelector(".rgb-color-container");
 const rgbColorValue = document.querySelector(".rgb-color-value");
+const rgbCopyBtn = document.querySelector(".rgb-copy-color");
 
 rgbBtn.addEventListener("click", () => {
   let extractRedValue = getRedInputRange.value;
@@ -36,3 +39,17 @@ rgbBtn.addEventListener("click", () => {
   rgbColorContainer.style.backgroundColor = rgbColor;
   rgbBtn.style.color = rgbColor;
 });
+
+function copyHexColorToClipBoard() {
+  navigator.clipboard.writeText(hexColorValue.textContent);
+  alert("Hex Color is copied to clipboard");
+}
+
+hexCopyBtn.addEventListener("click", copyHexColorToClipBoard);
+
+function copyRgbColorToClipBoard() {
+  navigator.clipboard.writeText(rgbColorValue.textContent);
+  alert("RGB Color is copied to clipboard");
+}
+
+rgbCopyBtn.addEventListener("click", copyRgbColorToClipBoard);
