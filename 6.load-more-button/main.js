02404ce055c439ef72bad4999cb4b9648621e3a6
Govind Rajput <govindrajput9819@gmail.com>
2024-05-17 00:02:07 +0530

    project completed

--- 6.load-more-button/main.js
+++ 6.load-more-button/main.js
@@ -1,10 +1,18 @@
 const productsContainer = document.querySelector(".products-container");
+const loadMoreBtn = document.querySelector(".load-more-btn");
 
-async function fetchListOfProducts() {
+let currentStep = 0;
+
+async function fetchListOfProducts(getCurrentStep) {
   try {
-    const response = await fetch("https://dummyjson.com/products", {
-      method: "GET",
-    });
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${
+        getCurrentStep === 0 ? 0 : getCurrentStep * 10
+      }`,
+      {
+        method: "GET",
+      }
+    );
     const result = await response.json();
     console.log(result);
     if (result && result.products) {
@@ -44,7 +52,15 @@ function displayProducts(productList) {
     productItemWrapper.appendChild(productDescription);
 
     productsContainer.appendChild(productItemWrapper);
+
+    if (productsContainer.children.length === 100) {
+      loadMoreBtn.setAttribute("disabled", "true");
+    }
   });
 }
 
-fetchListOfProducts();
+fetchListOfProducts(currentStep);
+
+loadMoreBtn.addEventListener("click", () => {
+  fetchListOfProducts((currentStep += 1));
+});
