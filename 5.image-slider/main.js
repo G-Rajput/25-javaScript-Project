8f8167f8954393a4e5e1fdb679d681cac110000b
Govind Rajput <govindrajput9819@gmail.com>
2024-05-16 19:47:58 +0530

    Image Slider sliding functionality done

--- 5.image-slider/main.js
+++ 5.image-slider/main.js
@@ -30,10 +30,67 @@ function displayImages(getImagesList) {
   dotsContainer.innerHTML = getImagesList
     .map(
       (item, index) =>
-        `<span class='dot' data-slide=${index}>
+        `<span class='dot' ${index === 0 ? "active" : ""} data-slide=${index}>
  </span>`
     )
     .join(" ");
 }
 
 fetchListOfImages();
+
+//slider functionality begins
+
+setTimeout(() => {
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let currentSlide = 0;
+
+  function activeDot(slide) {
+    document
+      .querySelectorAll(".dot")
+      .forEach((dotItem) => dotItem.classList.remove("active"));
+    document
+      .querySelector(`.dot[data-slide='${slide}']`)
+      .classList.add("active");
+  }
+
+  function changeCurrentSlide(currentSlide) {
+    slides.forEach(
+      (slideItem, index) =>
+        (slideItem.style.transform = `translateX(${
+          100 * (index - currentSlide)
+        }%)`)
+    );
+  }
+
+  changeCurrentSlide(currentSlide);
+
+  nextBtn.addEventListener("click", () => {
+    currentSlide++;
+
+    if (slides.length - 1 < currentSlide) {
+      currentSlide = 0;
+    }
+    changeCurrentSlide(currentSlide);
+    activeDot(currentSlide);
+  });
+
+  prevBtn.addEventListener("click", () => {
+    currentSlide--;
+
+    if (0 > currentSlide) {
+      currentSlide = slides.length - 1;
+    }
+    changeCurrentSlide(currentSlide);
+    activeDot(currentSlide);
+  });
+
+  dotsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("dot")) {
+      const currentSlide = event.target.dataset.slide;
+      changeCurrentSlide(currentSlide);
+      activeDot(currentSlide);
+    }
+  });
+}, 1000);
