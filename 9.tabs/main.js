2f9370329c863dd1346b1defae269c5445bcd5c1
Govind Rajput <govindrajput9819@gmail.com>
2024-05-17 17:21:43 +0530

    project done

---
+++ 9.tabs/main.js
@@ -0,0 +1,21 @@
+const tabContainer = document.querySelector(".container");
+const tabs = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".content");
+
+tabContainer.addEventListener("click", (event) => {
+  console.log(event.target.dataset);
+  const currentId = event.target.dataset.id;
+
+  if (currentId) {
+    tabs.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    event.target.classList.add("active");
+
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    const currentElement = document.getElementById(currentId);
+    currentElement.classList.add("active");
+  }
+});
