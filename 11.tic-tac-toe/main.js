92204fc10bff0ab598585a592227e8269a8848cc
Govind Rajput <govindrajput9819@gmail.com>
2024-05-18 23:28:13 +0530

    node element excessed

---
+++ 11.tic-tac-toe/main.js
@@ -0,0 +1,4 @@
+const board = document.querySelector(".board");
+const squares = document.querySelectorAll(".square");
+const message = document.querySelector(".message");
+const restartBtn = document.querySelector(".restart-btn");
