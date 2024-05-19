ed6f12b0e7d5565f5ae40fe507d55f5918aed1fc
Govind Rajput <govindrajput9819@gmail.com>
2024-05-19 12:03:01 +0530

    logic added

---
+++ 12.filter-card/main.js
@@ -0,0 +1,114 @@
+const categories = ["All", "Men", "Women", "kids"];
+const content = [
+  {
+    id: "Men",
+    label: "Men Shirt 1",
+  },
+  {
+    id: "Men",
+    label: "Men Shirt 2",
+  },
+  {
+    id: "Men",
+    label: "Men Shirt 3",
+  },
+  {
+    id: "Men",
+    label: "Men Shirt 4",
+  },
+  {
+    id: "Men",
+    label: "Men Shirt 5",
+  },
+  {
+    id: "Women",
+    label: "Women Shirt 1",
+  },
+  {
+    id: "Women",
+    label: "Women Shirt 2",
+  },
+  {
+    id: "Women",
+    label: "Women Shirt 3",
+  },
+  {
+    id: "Women",
+    label: "Women Shirt 4",
+  },
+  {
+    id: "Women",
+    label: "Women Shirt 5",
+  },
+  {
+    id: "Kids",
+    label: "Kids Shirt 3",
+  },
+  {
+    id: "Kids",
+    label: "Kids Shirt 4",
+  },
+  {
+    id: "Kids",
+    label: "Kids Shirt 5",
+  },
+  {
+    id: "Kids",
+    label: "Kids Shirt 10",
+  },
+  {
+    id: "Kids",
+    label: "Kids Shirt 20",
+  },
+];
+
+const filterButtons = document.querySelector(".filter-buttons-wrapper");
+const contentWrapper = document.querySelector(".content-wrapper");
+
+function createCatagory() {
+  categories.forEach((category) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = category;
+    buttonElement.classList.add("filter-button");
+    buttonElement.setAttribute("data-filter", category);
+    filterButtons.appendChild(buttonElement);
+  });
+}
+
+function createContent() {
+  content.forEach((contentItem) => {
+    const singleContentItem = document.createElement("div");
+    singleContentItem.classList.add("card", contentItem.id);
+    singleContentItem.textContent = contentItem.label;
+
+    contentWrapper.appendChild(singleContentItem);
+  });
+}
+
+createCatagory();
+createContent();
+
+const allFilterButtons = document.querySelectorAll(".filter-button");
+
+const allCards = document.querySelectorAll(".card");
+
+function filterCardsByCategory(extractCurrentCategory, allCards) {
+  allCards.forEach((item) => {
+    const isShowAllCards = extractCurrentCategory.toLowerCase() === "all";
+    const isItemFiltered = !item.classList.contains(extractCurrentCategory);
+
+    if (isItemFiltered && !isShowAllCards) {
+      item.classList.add("hide");
+    } else {
+      item.classList.remove("hide");
+    }
+  });
+}
+
+allFilterButtons.forEach((singleFilterButtonItem) => {
+  singleFilterButtonItem.addEventListener("click", () => {
+    const extractCurrentCategory = singleFilterButtonItem.dataset.filter;
+    console.log(extractCurrentCategory);
+    filterCardsByCategory(extractCurrentCategory, allCards);
+  });
+});
